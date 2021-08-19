4e3c2dce857035b0ac3acdf42574e66fd6563190
SanjarbekSaminjov <sanjarbeksaminjonovv@gmail.com>
2021-08-19 13:31:31 +0500

    first commit

---
+++ photos/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+
+def addPhoto(request):
+    return render(request, 'photos/add.html')
+
+
+def viewPhoto(request, pk):
+    return render(request, 'photos/photo.html')
